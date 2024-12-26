0bba0ae6d739ca9150014f6b7c219d6a877c9aee
Aaditya Saxena <111501333+StarTitan2004@users.noreply.github.com>
2024-12-26 18:34:37 +0530

    Add files via upload

---
+++ var_consts/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	cout<<"Aaj Mars wali vid banani h"<<endl;//endl= end line
+	int a;
+	a=2;
+	cout<<a<<endl;
+	
+	const int b=21; //now this value cannot be changed as we have given const keyword	
+	return 0;
+	
+	
+}
